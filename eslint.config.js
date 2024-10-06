0059ad7351b2680af1d626869c8da23e801728c0
Hen <entidad.estelar@gmail.com>
2024-10-06 18:41:23 -0300

    chore: added Pretiier and ESLint compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,18 @@
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      ...pluginJs.configs.recommended.rules, "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-    }
+      ...pluginJs.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
   },
 ];
