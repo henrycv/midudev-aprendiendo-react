9c1c59e6b07f88847e3c4976f99e3f8fc1af5944
Hen <entidad.estelar@gmail.com>
2024-10-06 18:04:44 -0300

    fix: ESLint JSX error

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,12 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      ...pluginJs.configs.recommended.rules, "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    }
+  },
 ];
